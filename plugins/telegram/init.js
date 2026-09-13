5087cc12166a7f608d3cbda5142e7884ff33e750
drrako <drrako@proton.me>
2026-09-13 12:59:17 +0300

    Revert token status sizing override

--- plugins/telegram/init.js
+++ plugins/telegram/init.js
@@ -94,7 +94,7 @@ plugin.onLangLoaded = function() {
 				$('<div>').addClass('col-12 col-md-3').append($('<label>').attr({for: 'telegram_token'}).text(theUILang.telegramBotToken)),
 				$('<div>').addClass('col-12 col-md-5').append(
 					$('<input>').attr({type: 'password', id: 'telegram_token', autocomplete: 'new-password', maxlength: 256}),
-					$('<small>').attr({id: 'telegram_token_status'}).addClass('form-text').css({color: 'var(--text-color, inherit)', display: 'block', width: '100%', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'})
+					$('<small>').attr({id: 'telegram_token_status'}).addClass('form-text')
 				)
 			),
 			$('<div>').addClass('row').append(
